37d90af179dc4292d79e7a760f68cbdbfedca89b
Hector-bit <hectormartinez.2978@gmail.com>
2020-03-02 10:11:47 -0800

    login route set up

--- mud_game/src/App.js
+++ mud_game/src/App.js
@@ -1,23 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import Loginpage from './components/loginpage'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      </div>
+      <switch>
+        <Route exact path="/login" component={Loginpage}/>
+      </switch>
+      </Router>
       </header>
     </div>
   );
