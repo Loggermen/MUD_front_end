6c1292bfa2b6aab1468776bd17e6c089fa127e65
Taylor <52665999+taylorhh5@users.noreply.github.com>
2020-03-03 19:34:07 -0500

    Working on map component

--- mud_game/src/components/game.js
+++ mud_game/src/components/game.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Controls from './Controls.js'
+import Map from './Map.js'
 
 function Game() {
     return(
         <div>
         <div>Hello</div>
+        <Map/>
         <Controls/>
         </div>
     )
